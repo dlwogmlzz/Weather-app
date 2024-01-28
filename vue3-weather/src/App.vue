dc8fb91472c1d247a4562b3d96823dfa6cb0379d
dlwogmlzz <lwwljh90@gmail.com>
2024-01-28 17:41:39 +0900

    날씨앱 화면 UI작성

---
+++ vue3-weather/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <router-view /> -->
+  <RouterView />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue3-weather/src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+	<!-- wrap, container를 만드는이유 : 가장 바깥쪽 레이아웃의 큰 틀을 만들어주는 작업이다. -->
+  <div class="wrap">
+		<div class="container">
+			<MainView />
+		</div>
+	</div>
+</template>
+
+<script>
+import MainView from '../components/MainView.vue'
+export default {
+  components: { MainView },
+
+}
+</script>
+
+<style lang="scss" scoped>
+	@import '../scss/main.scss';
+
+	.wrap {
+		@include center;
+		width: 100%;
+		height: 100vh;
+		font-size: 16px;
+
+		.container {
+			@include center-sb;
+			width: 55%;
+			height: 90%;
+			max-width: 1920px;
+			max-height: 1354px;
+		}
+	}
+</style>
